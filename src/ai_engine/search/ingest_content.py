e10a6d07016c3250e35ad856690a74fa30fa0709
dabadav <dante.avino@alum.esci.upf.edu>
2025-11-24 00:21:30 +0100

    ingest content pipeline

--- src/ai_engine/search/ingest_content.py
+++ src/ai_engine/search/ingest_content.py
@@ -18,6 +18,7 @@ from ai_engine.common import Item
 
 OMEKA_DATA_PATH = "../../../data/omeka_data.parquet"
 FILTER_ITEMS_PATH = "../../../data/test_items.json"
+TOPIC_BERT_PATH = "../../../data/documents.json"
 
 with open(FILTER_ITEMS_PATH, 'r') as f:
     filter_ids_dict = json.load(f)
@@ -42,6 +43,7 @@ if __name__ == '__main__':
     ####  Omeka  #####
     ##################
     df = pd.read_parquet("../../../data/omeka_data.parquet")
+    documents = {'items': {}}
 
     # Init Model
     logger.info(f"Initializing embedding model: {EMBEDDING_MODEL}")
@@ -65,75 +67,84 @@ if __name__ == '__main__':
             logger.info(f"Skipping index {index} (ID: {row['id']}): Less than 5 words in text_all.")
             continue
 
+        if (len(item.text_all.split()) < 5):
+            logger.info(f"Skipping index {index} (ID: {row['id']}): Less than 5 words in text_all.")
+            continue
+
         # Fix field 
         # TODO: fic in the omeka_data already
         if item.creator == "unbekannt":
             item.creator = ""
 
+        documents['items'][row['id']] = item.text_all
         # --- Filtering Logic Ends Here ---
 
         # logger.info(f"embedding text for item {item.id}: {item.text_all}")
-        encoding_text = f"{item.text_all} - {item.creator}"
-        embedding = model.encode(encoding_text, show_progress_bar=False).tolist()
-        payload = asdict(item)
-        payload["image_url"] = item.image_url
-        try:
-            payload = json.loads(json.dumps(payload, default=to_serializable))
-        except TypeError as e:
-            # A cleaner error handler if the json module itself fails to serialize a weird type
-            logger.error(f"Failed to serialize payload for Item ID: {item.id}. Error: {e}")
-            logger.info(item)
-            continue # Skip this item or handle the error appropriately
+        # encoding_text = f"{item.text_all} - {item.creator}"
+        # embedding = model.encode(encoding_text, show_progress_bar=False).tolist()
+        # payload = asdict(item)
+        # payload["image_url"] = item.image_url
+        # try:
+        #     payload = json.loads(json.dumps(payload, default=to_serializable))
+        # except TypeError as e:
+        #     # A cleaner error handler if the json module itself fails to serialize a weird type
+        #     logger.error(f"Failed to serialize payload for Item ID: {item.id}. Error: {e}")
+        #     logger.info(item)
+        #     continue # Skip this item or handle the error appropriately
 
-        points.append(
-            PointStruct(
-                id=row['id'],    #str(uuid4()),  # or use row["id"] if it's unique
-                vector=embedding,
-                payload=payload
-            )
-        )
+        # points.append(
+        #     PointStruct(
+        #         id=row['id'],    #str(uuid4()),  # or use row["id"] if it's unique
+        #         vector=embedding,
+        #         payload=payload
+        #     )
+        # )
+
+    with open(TOPIC_BERT_PATH, "w") as f:
+        json.dump(documents, f, indent=4)
+    
     logger.info(f"Total points prepared for upsert: {len(points)}")
 
     #################
     #### Qdrant  ####
     #################
 
-    # Init client
-    logger.info(f"Qdrant client initialized for URL: {QDRANT_API_URL}")
-    client = QdrantClient(
-        url=QDRANT_API_URL,
-        api_key=QDRANT_API_KEY
-    )
+    # # Init client
+    # logger.info(f"Qdrant client initialized for URL: {QDRANT_API_URL}")
+    # client = QdrantClient(
+    #     url=QDRANT_API_URL,
+    #     api_key=QDRANT_API_KEY
+    # )
 
-    vector_size = len(points[0].vector) if points else model.get_sentence_embedding_dimension()
-    logger.info(f"Preparing Qdrant collection '{COLLECTION_NAME}' with vector size {vector_size}")
+    # vector_size = len(points[0].vector) if points else model.get_sentence_embedding_dimension()
+    # logger.info(f"Preparing Qdrant collection '{COLLECTION_NAME}' with vector size {vector_size}")
 
-    # Check if collection exists
-    if not client.collection_exists(collection_name=COLLECTION_NAME):
-        client.create_collection(
-            collection_name=COLLECTION_NAME,
-            vectors_config=VectorParams(size=vector_size, distance=Distance.COSINE)
-        )
+    # # Check if collection exists
+    # if not client.collection_exists(collection_name=COLLECTION_NAME):
+    #     client.create_collection(
+    #         collection_name=COLLECTION_NAME,
+    #         vectors_config=VectorParams(size=vector_size, distance=Distance.COSINE)
+    #     )
 
-    logger.info(f"Starting upsert of {len(points)} vectors to '{COLLECTION_NAME}'")
-    client.upsert(
-        collection_name=COLLECTION_NAME,
-        points=points
-    )
-    logger.info("Upsert completed successfully.")
+    # logger.info(f"Starting upsert of {len(points)} vectors to '{COLLECTION_NAME}'")
+    # client.upsert(
+    #     collection_name=COLLECTION_NAME,
+    #     points=points
+    # )
+    # logger.info("Upsert completed successfully.")
 
-    logger.info("Creating payload index on 'time_metadata.dates_of_creation'")
-    client.create_payload_index(
-        collection_name=COLLECTION_NAME,
-        field_name="time_metadata.dates_of_creation",
-        field_schema="datetime"
-    )
-    client.create_payload_index(
-        collection_name=COLLECTION_NAME,
-        field_name="locations",
-        field_schema="geo",
-    )
+    # logger.info("Creating payload index on 'time_metadata.dates_of_creation'")
+    # client.create_payload_index(
+    #     collection_name=COLLECTION_NAME,
+    #     field_name="time_metadata.dates_of_creation",
+    #     field_schema="datetime"
+    # )
+    # client.create_payload_index(
+    #     collection_name=COLLECTION_NAME,
+    #     field_name="locations",
+    #     field_schema="geo",
+    # )
 
-    logger.info("Payload index created.")
-    logger.info("Data ingestion process completed.")
+    # logger.info("Payload index created.")
+    # logger.info("Data ingestion process completed.")
 
